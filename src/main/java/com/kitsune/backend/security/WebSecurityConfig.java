6606ff0dc4d35705934c8bdaf3a312cf2b7bb5dc
oOBoomberOo <boomberisalreadytaken@gmail.com>
2024-06-18 11:28:19 +0900

    added cors allowed origins

--- src/main/java/com/kitsune/backend/security/WebSecurityConfig.java
+++ src/main/java/com/kitsune/backend/security/WebSecurityConfig.java
@@ -8,11 +8,13 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @Slf4j
 @RequiredArgsConstructor
-public class WebSecurityConfig {
+public class WebSecurityConfig implements WebMvcConfigurer {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -25,5 +27,9 @@ public class WebSecurityConfig {
                 .build();
     }
 
-
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173", "https://music.boomber.xyz");
+    }
 }
